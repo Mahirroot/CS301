d43305fdcb0d6084d9e807f6c971998e09e0d9aa
Mahirroot <samillimahir0@mail.ru>
2024-01-19 16:58:51 +0400

    feat : add script.js and index.html

--- classwork/script.js
+++ classwork/script.js
@@ -49,3 +49,19 @@
     
 // }
 // console.log(sum);
+
+// const username = document.querySelector('#username')
+// const password = document.querySelector('#password')
+// const email = document.querySelector('#email')
+// const myform = document.querySelector('#myform')
+
+
+// myform.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     const user = {
+//         username :username.value ,
+//         email:email.value, 
+//         password : password.value
+//     }
+//     console.log(user);
+// })
